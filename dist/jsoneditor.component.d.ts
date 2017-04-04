dc6f9c195ef0710dfb8265dfe89e8be49e9a846b
Scott Hankinson <scott.hankinson@slalom.com>
2017-04-04 16:01:54 -0400

    Tweaking ignore

---
+++ dist/jsoneditor.component.d.ts
@@ -0,0 +1,23 @@
+import { ElementRef } from '@angular/core';
+export declare class JsonEditorComponent {
+    private rootElement;
+    private editor;
+    private optionsDiffer;
+    private dataDiffer;
+    private options;
+    private data;
+    constructor(rootElement: ElementRef);
+    ngOnInit(): void;
+    collapseAll(): void;
+    destroy(): void;
+    expandAll(): void;
+    focus(): void;
+    set(json: JSON): void;
+    setMode(mode: string): void;
+    setName(name: string): void;
+    setSchema(schema: any): void;
+    get(): JSON;
+    getMode(): string;
+    getName(): string;
+    getText(): string;
+}
---
+++ dist/jsoneditor.options.d.ts
@@ -0,0 +1,19 @@
+export declare class JsonEditorOptions {
+    ace: Object;
+    ajv: Object;
+    onChange: Function;
+    onEditable: Function;
+    onError: Function;
+    onModeChange: Function;
+    escapeUnicode: boolean;
+    sortObjectKeys: boolean;
+    history: boolean;
+    mode: String;
+    modes: String[];
+    name: String;
+    schema: Object;
+    search: boolean;
+    indentation: Number;
+    theme: Number;
+    constructor();
+}
